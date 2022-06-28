efb4823032323ee01b1f1896cbe14b483bf21cc1
SureshChidurala <suresh@fabstudio.co>
2022-06-28 07:55:14 +0530

    #004 Fixed eslint issues

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+import StorybookUI from './storybook';
+import Config from 'react-native-config';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () =>
+  Config.LOAD_STORYBOOK === 'true' ? StorybookUI : App,
+);
